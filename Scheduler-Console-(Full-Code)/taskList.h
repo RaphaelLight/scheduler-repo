8fc3098848a9aa91c760a87dcf52cba58fd8a611
RaphaelLight <115327472+RaphaelLight@users.noreply.github.com>
2022-10-24 20:59:19 +0800

    Update taskList.h

--- Scheduler-Console-(Full-Code)/taskList.h
+++ Scheduler-Console-(Full-Code)/taskList.h
@@ -11,15 +11,17 @@ class taskList : public unorderedLinkedList<Task>
 {
 public:
     //Function to add task to the task list.
-    void addTask(const Task&);
+    void addTask(const Task &);
     //Function to remove task on the task list.
     void removeTask(int);
     //Function to clear tasks on the task list.
     void clearTasks();
     //Function to copy tasks of another list to this list.
     void copyTasks(const taskList&);
-    //Function to move a task to the top of the list.
+    //Function to move a task to the front of the list.
     void moveTaskToFront(int);
+    //Function to move a task to the back of the list.
+    void moveTaskToBack(int);
     //Function to move task up one step in the list.
     void moveTaskUp(int);
     //Function to move task down one step in the list.
@@ -56,7 +58,13 @@ void taskList::moveTaskToFront(int n)
 {
     moveToFront(n);
     //Calls the moveToFront function from the linked list class
-    //to move a task to the top of the list.
+    //to move a task to the front of the list.
+}
+void taskList::moveTaskToBack(int n)
+{
+    moveToBack(n);
+    //Calls the moveToBack function from the linked list class
+    //to move a task to the back of the list.
 }
 void taskList::moveTaskUp(int n)
 {
